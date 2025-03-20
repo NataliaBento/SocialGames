15658b10c10c58bd170201a3e639f0e616028a64
Natalia <nataliabento97@gmail.com>
2025-03-20 14:31:14 -0300

    feat: adiciona a rota de categorias, e inicia middleares no app.js

--- app.js
+++ app.js
@@ -4,9 +4,24 @@ const handlebars = require('express-handlebars')
 const app = express()
 const admin = require('./routes/admin')
 const path = require('path')
-//const mongoose = require ('mongoose')
+const mongoose = require ('mongoose')
+const session = require ('express-session')
+const flash = require ('connect-flash')
 // Configurações 
-    // Body Parser 
+
+    // Sessão
+    app.use(session({
+        secret: "aprendendoefazendo",
+        resave: true,
+        saveUninitialized: true
+    } ))
+    app.use(flash())
+    //Middleware
+    app.use(function(req, res, next){
+        res.locals.success_msg = req.flash('success_msg')
+        res.locals.error_msg = req.flash('error_msg')
+        next();
+    })
 
     app.use(express.urlencoded({extended: true}))
     app.use(express.json())
@@ -16,6 +31,14 @@ const path = require('path')
         partialsDir: path.join(__dirname, 'views/partials')
     }))
     app.set('view engine', 'handlebars')
+
+    //Mongoose
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://localhost/blogapp').then(function(){
+        console.log('Mongo conectado com sucesso!')
+    }).catch(function(err) {
+        console.log('Erro ao se conectar com o mongo'+err)
+    })
 
     //Public 
     app.use(express.static(path.join(__dirname, 'public')))
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,8 @@
 const express = require ('express')
 const router = express.Router()
+const mongoose = require ('mongoose')
+require('../models/Categoria')
+const Categoria = mongoose.model("categorias")
 
 router.get('/', function(req, res){
     res.render('admin/index')
@@ -17,4 +20,16 @@ router.get('/categorias/add', function(req, res){
     res.render('admin/addcategorias')
 })
 
+router.post('/categorias/nova', function(req, res){
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+
+        new Categoria(novaCategoria).save().then(function(){
+            console.log('Categoria salva com sucesso!')
+        }).catch(function(err){
+            console.log('Erro ao cadastrar categoria'+err)
+        })
+})
 module.exports = router
