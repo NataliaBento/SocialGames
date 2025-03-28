fc4144f07fb3292339e37d8fee1bae0f71f3078a
Natalia <nataliabento97@gmail.com>
2025-03-28 14:01:25 -0300

    feat: cria rota para deletar postagens

--- routes/admin.js
+++ routes/admin.js
@@ -193,4 +193,15 @@
             res.redirect('/admin/postagens')
         })
     })
+
+    router.get('/postagens/deletar/:id', (req, res) => {
+        Postagem.deleteOne({_id: req.params.id}).then(()=> {
+            req.flash('success_msg',`Postagem deletada com sucesso`)
+            res.redirect('/admin/postagens')
+        }).catch((err) => {
+            req.flash('error_msg','Houve um erro ao deletar a postagem')
+            console.log("Erro ao deletar: ",err)
+            res.redirect('/admin/postagens')
+        })
+    })
     module.exports = router
