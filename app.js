59841feb839edc8957b259b62e331dd071b98545
Natalia <nataliabento97@gmail.com>
2025-03-31 11:10:27 -0300

    feat: cria pagina de postagens, botão de ler mais e rota de pagina de postagens

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const path = require('path')
 const mongoose = require ('mongoose')
 const session = require ('express-session')
 const flash = require ('connect-flash')
+require('./models/Postagem')
+const Postagem = mongoose.model('postagens')
 // Configurações 
 
     // Sessão
@@ -43,6 +45,39 @@ const flash = require ('connect-flash')
     //Public 
     app.use(express.static(path.join(__dirname, 'public')))
 // Rotas 
+
+    app.get('/', (req, res) => {
+        Postagem.find().lean().populate('categoria').sort({data: 'desc'}).then((postagens) => {
+            res.render('index', {postagens: postagens})
+        }).catch((err) => {
+            req.flash('error_msg', 'Houve um erro interno')
+            res.redirect('/404')
+        })
+        
+    })
+
+    app.get('/postagem/:slug', (req, res)=>{
+        Postagem.findOne({slug: req.params.slug}).lean().then((postagem)=>{
+            if(postagem){
+                res.render('postagem/index', {postagem:postagem})
+            }else{
+                req.flash('error_msg', "Essa postagem não existe")
+                res.redirect('/')
+            }
+        }).catch((err) => {
+            req.flash('error_msg','Houve um erro interno')
+            res.redirect('/')
+        })
+    })
+
+    app.get('/404', (req, res) => {
+        res.send('Erro 404!')
+    })
+
+    app.get('/posts', (req, res) => {
+        res.send('Lista Posts')
+    })
+
     app.use('/admin', admin)
 
 // Outros 
