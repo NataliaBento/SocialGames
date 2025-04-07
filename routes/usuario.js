a6174fa1c105b9f780190df55eb3ed47b30cdd9b
Natalia <nataliabento97@gmail.com>
2025-04-06 22:26:45 -0300

    feat: altera configuraçoes de admin

--- routes/usuario.js
+++ routes/usuario.js
@@ -49,6 +49,7 @@ router.post('/registro', (req, res) => {
                     nome:req.body.nome,
                     email: req.body.email,
                     senha: req.body.senha,
+                    eAdmin:1
 
                 })
 
